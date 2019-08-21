ef4560105d44c7643c2ddf0e1a5e80e51c5dc656
HcySunYang <HcySunYang@outlook.com>
2019-08-21 14:35:10 +0800

    refactor: publicPath
    affects: @vapper/webpack-config

--- packages/webpack-config/lib/client.js
+++ packages/webpack-config/lib/client.js
@@ -10,8 +10,9 @@ module.exports = (api, config) => {
     .when(!isProd, entry => entry.add(require.resolve('webpack-hot-middleware/client')))
     .add(api.resolveCore('app/clientEntry.js'))
 
+  const publicPath = config.output.get('publicPath')
   config.output
-    .publicPath('/_vapper_/')
+    .publicPath((isProd && publicPath) ? publicPath : '/_vapper_/')
 
   config
     .plugin('VueSSRClientPlugin')
