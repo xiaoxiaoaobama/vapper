616f344327d1e76e7458c11bce50c19dae409fe7
HcySunYang <HcySunYang@outlook.com>
2019-09-10 18:40:54 +0800

    fix(core): determine whether a resource is a static resource based on url.path
    affects: @vapper/core

--- packages/core/lib/plugins/serveStatic.js
+++ packages/core/lib/plugins/serveStatic.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const finalhandler = require('finalhandler')
 const serveStatic = require('serve-static')
+const Url = require('url-parse')
 
 module.exports = (api) => {
   const handler = (req, res, next) => {
     const originalUrl = req.url
 
     // FIX: Treat URLs with extensions as requests for static resources?
-    const hasExt = path.extname(req.url)
+    const url = new Url(req.url)
+    const hasExt = path.extname(url.pathname)
+
     if (hasExt) {
       req.url = req.url.replace(/^\/_vapper_/, '')
 
