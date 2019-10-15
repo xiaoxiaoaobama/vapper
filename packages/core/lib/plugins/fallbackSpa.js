038c37c01adeb3eab961b13240e09270bb6a0817
HcySunYang <HcySunYang@outlook.com>
2019-10-15 18:34:46 +0800

    feat: manually fall back to SPA mode
    affects: @vapper/core

--- packages/core/lib/plugins/fallbackSpa.js
+++ packages/core/lib/plugins/fallbackSpa.js
@@ -2,14 +2,20 @@ const serveStatic = require('serve-static')
 const finalhandler = require('finalhandler')
 
 module.exports = (api) => {
+  // Exposing the fallback function gives the user more choices
+  api.fallbackSPA = (req, res) => {
+    api._forceFallback = true
+    api.handler(req, res)
+  }
+
   const preHandler = (req, res, next) => {
     const meta = api.getRouteMeta(req.url)
     const needFallback = api.options.ssr
       ? (meta && meta.ssr === false)
       : (!meta || meta.ssr !== true)
 
-    if (needFallback) {
-      api.logger.debug(`Fall back SPA mode, url is: ${req.url}`)
+    if (needFallback || api._forceFallback) {
+      api.logger.debug(`${api._forceFallback ? 'Force' : ''} Fall back SPA mode, url is: ${req.url}`)
       fallBack(req, res)
       return
     }
