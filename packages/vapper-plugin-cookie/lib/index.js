c2842db9874767429da986079a17afb67eaaf067
HcySunYang <HcySunYang@outlook.com>
2020-02-21 10:45:31 +0800

    refactor: the cookie plugin does not need to be compiled
    affects: @vapper/plugin-cookie

--- packages/vapper-plugin-cookie/lib/index.js
+++ packages/vapper-plugin-cookie/lib/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = (api) => {
   api.addEnhanceFile({
+    needCompile: false,
     client: path.resolve(__dirname, './cookie.js'),
     server: path.resolve(__dirname, './cookie.js'),
     clientModuleName: 'cookie'
